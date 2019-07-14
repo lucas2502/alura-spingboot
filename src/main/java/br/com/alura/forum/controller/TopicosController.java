ac2da3621c022bda8e8b9a950bf8b53437d351eb
lucas2502 <lucas2502@live.com>
2019-07-14 19:41:37 -0300

    Registering topics

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -4,29 +4,42 @@ import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.alura.forum.controller.dto.TopicoDTO;
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.*;
 import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
+//Map de URL a ser chamada no navegador para class TopicosController
+@RequestMapping("/topicos")
 public class TopicosController {
 	
 	@Autowired
 	private TopicoRepository topicoRepository;
 	
 	// Método LISTAR 
-	@RequestMapping("/topicos")
-	public List<TopicoDTO> lista(String nomeCurso) {
+	@GetMapping//Chamada GET
+	public List<TopicoDto> lista(String nomeCurso) { //DTO dados que saem api e vão para o client
 		if(nomeCurso == null ) {
+			//Listando todos os topicos do banco de dados
 			List<Topico> topicos = topicoRepository.findAll();
-			return TopicoDTO.converter(topicos);
+			return TopicoDto.converter(topicos);
 		} else {
+			//Listando topicos do banco de dados por nomeCurso
 			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
-			return TopicoDTO.converter(topicos);
+			return TopicoDto.converter(topicos);
 		}
 	}
+	//Método inserir
+	@PostMapping//Chamada POST
+	public void cadastrar(TopicoForm topico) {//Form dados que chegam do client e vão para api
+		
+	}
+	
 }
--- src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
@@ -6,14 +6,14 @@ import java.util.stream.Collectors;
 
 import br.com.alura.forum.modelo.Topico;
 
-public class TopicoDTO {
+public class TopicoDto {
 	
 	private Long id;
 	private String titulo;
 	private String mensagem;
 	private LocalDateTime dataCriacao;
 	
-	public TopicoDTO(Topico topico) {
+	public TopicoDto(Topico topico) {
 		this.id = topico.getId();
 		this.titulo = topico.getTitulo();
 		this.mensagem = topico.getMensagem();
@@ -34,9 +34,9 @@ public class TopicoDTO {
 		return dataCriacao;
 	}
  
-	public static List<TopicoDTO> converter(List<Topico> topicos) {
+	public static List<TopicoDto> converter(List<Topico> topicos) {
 		// TODO Auto-generated method stub
-		return topicos.stream().map(TopicoDTO::new).collect(Collectors.toList());
+		return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
 	} 
 	
 	
